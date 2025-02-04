5f80ed6aba1dd3d2fa2df513b5013fb469a7f3ba
Ketan Pipaliya <34193142+ketanpipaliya@users.noreply.github.com>
2025-02-04 06:36:03 +0000

    HelloWorld

---
+++ Program.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace Demo_04_Feb_2025
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
